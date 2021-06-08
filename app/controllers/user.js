d8e6b6a9fd598fbc5feaf7550599f5c9ac6958a9
Pranil Kakade <pranilkakade111@gmail.com>
2021-06-08 17:14:34 +0530

    Added: Added Controller file for user/admin registration

---
+++ app/controllers/user.js
@@ -0,0 +1,63 @@
+/** ***********************************************************************
+ * Execution        : 1. default node       cmd> nodemon server.js
+ *
+ * Purpose          : To hit the perticular API 
+
+ * @file            : user.js
+ * @author          : Pranil Kakade
+ * @version         : 1.0
+ * @since           : 06-05-2021
+ *
+ ************************************************************************* */
+const joi = require('@hapi/joi');
+const userService = require('../services/user');
+
+const requestValidationSchema = joi.object({
+  firstName: joi.string().min(3).max(10).pattern(new RegExp('^[A-Z]{1}[a-z]{2,}$')).message('First letter Should be capital For first name').required(),
+  lastName: joi.string().min(3).max(10).pattern(new RegExp('^[A-Z]{1}[a-z]{2,}$')).message('First letter Should be capital For last name').required(),
+  email: joi.string().email().message('Email Should Be in Proper Manner').required(),
+  password: joi.string().required(),
+  role: joi.string().required(),
+});
+
+class UserController {
+   /**
+   * @description add user/admin to database
+   * @param {*} req in json formate
+   * @param {*} res sends response from server
+   */
+  userRegister = (req, res) => {
+    if (!req.body.firstName || !req.body.lastName || !req.body.email || !req.body.password) {
+        return res.status(400).send({
+            success: false,
+            message: "Fileds Cannot Be Empty...!!!",
+        });
+    }
+    const result = requestValidationSchema.validate(req.data);
+    if (result.error){
+        return res.send('Enter the Valid Data..!');
+    }
+
+    const userDetails = {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        password: req.body.password,
+        role: req.role,
+    };
+    userService.userRegister(userDetails, (err, userResult) => {
+        err
+           ? res.status(401).send({
+            success: false,
+            message: err.message,
+           })
+           : res.status(200).send({
+               success: true,
+               message: 'Data Inserted Successfully..!',
+               data: userResult,
+           });
+    });
+  };
+};
+
+module.exports = new UserController();
