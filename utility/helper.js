27616d22927c3b33e8c1d1e0e280691860c76ce5
Pranil Kakade <pranilkakade111@gmail.com>
2021-06-10 10:14:47 +0530

    Add: Create Token Function For Login User/Admin

--- utility/helper.js
+++ utility/helper.js
@@ -1,7 +1,29 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
 class Helper {
- verifyRole = (role) => (req, res, next) => {
-  req.role = role;
-  next();
+ verifyRole = (permission) => (req, res, next) => {
+  req.role = permission;
+  if(permission.includes(req.role))
+  {
+    next();
+  } else {
+    return res.status(401).send({
+      success: false,
+      message: 'You Dont Have Permission..!'
+    });
+  }
+
  };
+
+ createToken = (data) => {
+   const token = jwt.sign({ 
+    email: data.email,
+    id: data._id,
+    role: data.role 
+  }, process.env.JWT, { expiresIn: '24d'});
+  return token;
+ };
+
 };
 module.exports = new Helper();
