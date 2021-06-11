80b85d691b63c6606a7b8fdf3ea820a8b4a8b722
Pranil Kakade <pranilkakade111@gmail.com>
2021-06-11 19:49:51 +0530

    add: Add VerifyToken Middleware For authorization Purpose In Book Store App

--- utility/helper.js
+++ utility/helper.js
@@ -1,4 +1,7 @@
 const jwt = require('jsonwebtoken');
+const ejs = require('ejs');
+const nodemailer = require('nodemailer');
+const logger = require('../Logger/logger');
 require('dotenv').config();
 
 class Helper {
@@ -37,7 +40,70 @@ class Helper {
   return token;
  };
 
- 
+ nodeMail = (data) => {
+  const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.USER,
+      pass: process.env.PASS,
+    },
+  });
+ ejs.renderFile('app/views/nodeMail.ejs', 'utf8', (error, info) => {
+  if (error) {
+    logger.log('error', error);
+  } else {
+    const mailOption = {
+      from: 'pranilkakade2@gmail.com',
+      to: data.email,
+      subject: 'Reset The Password',
+      html: `${info}<button><a href="${'http://localhost:8000/resetPassword/'}${this.createToken(data)}">Button</a></button>`,
+    };
+    transporter.sendMail(mailOption, (err, result) => {
+      if (err) {
+        logger.log('error', err);
+      } else {
+        logger.log('info', result);
+      }
+    });
+  }
+});
+};
+
+verifyToken = (req, res, next) => {
+  try {
+    const decode = jwt.verify(req.headers.token, process.env.JWT);
+    req.userData = decode;
+    const userId = decode.id;
+    req.userId = userId;
+    const userRole = decode.role;
+    req.userRole = userRole;
+    next();
+  } catch (error) {
+    res.status(401).send({
+      error: 'Unauthorized....!!! !',
+    });
+  }
+};
+verifyRole = (req, res, next) => {
+  try {
+    const decode = jwt.verify(req.headers.token, process.env.JWT);
+    const userRole = decode.role;
+    req.userRole = userRole;
+    if(decode.role !== 'admin') {
+      res.status(501).send({
+        success: false,
+        message: 'Authorization Failed..!'
+      });
+    }
+    next();
+  } catch (error) {
+    res.status(401).send({
+      error: 'Unauthorized....!!! !',
+    });
+  }
+};
+
+
 
 };
 module.exports = new Helper();
