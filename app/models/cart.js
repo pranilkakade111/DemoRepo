75a1f98873dab564d47f71e5f2081c94129edd23
Pranil Kakade <pranilkakade111@gmail.com>
2021-06-22 10:32:25 +0530

    add: Database Schema In Models File For Purchase Book From Cart API

--- app/models/cart.js
+++ app/models/cart.js
@@ -13,10 +13,10 @@ const cartSchema = mongoose.Schema({
 const Cart = mongoose.model('Cart', cartSchema);
 
 class CartModel {
+
   checkBook = (bookID, callback) => {
-    
-     Cart.find({ bookId: bookID.bookId }, (err, bookResult) => {
-       if (err){
+     Cart.findOne({ bookId: bookID.bookId }, (err, bookResult) => {
+       if (err) {
          callback(err, null);
        } else {
          callback(null, bookResult);
@@ -27,19 +27,16 @@ class CartModel {
   addToCart = async (cartData, callback) => {
     const userId = cartData.userId;
     const bookId = cartData.bookId;
+    
     const userPresent = await Cart.findOne({userId: userId});
-    console.log(userPresent);
      if(!userPresent) {
       const cart = new Cart({
         bookId: cartData.bookId,
         userId: cartData.userId,
     });
-    cart.save(callback);
-     } else {
-      const cartCheck = await Cart.findOneAndUpdate(userId, { $addToSet: { bookId: bookId } }, {new: true});
-       console.log(userId);
-      // console.log(bookId);
-      console.log(cartCheck);
+      cart.save(callback);
+    } else {
+      const cartCheck = await Cart.updateOne({userId}, { $addToSet: { bookId: bookId } });
       callback(null, cartCheck);
       }
   };
@@ -48,18 +45,29 @@ class CartModel {
     const bookId = cartDetail.bookId;
     const userId = cartDetail.userId;
 
-    const removeBook = await Cart.findOneAndUpdate (userId, { $pull: { bookId: bookId } });
+    const removeBook = await Cart.updateOne({userId}, { $pull: { bookId: bookId } });
     callback(null, removeBook);
   };
 
-  purchaseBook = (userID, callback) => {
-    Cart.findOneAndUpdate (userID, { isPurchased: false }, { new: true })
-    .then((user) => {
-      callback(null, user);
-    }).catch((err) => {
-      callback(err, null);
+  validPlaceOrder = (cart, callback) => {
+    Cart.find({ cartId: cart.cartId }, (err, validCheck) => {
+      console.log(cart.userId);
+      if(cart.userId) {
+        callback(null, validCheck);
+      } else {
+        callback(err, null);
+      }
     });
   };
+
+  purchaseBook = (cart) => {
+    return new Promise((resolve, reject) => {
+      Cart.findByIdAndUpdate(cart.cartId, {isPurchased: true}, {new: true})
+      .then((data) => resolve({ data }))
+      .catch((err) => reject({ err }));
+    });
+  
+ };
 };
 
 module.exports = new CartModel();
