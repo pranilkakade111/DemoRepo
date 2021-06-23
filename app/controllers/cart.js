16369df5e65c5a3a53fc0f14f748b1ece0e2a4fd
Pranil Kakade <pranilkakade111@gmail.com>
2021-06-24 02:08:12 +0530

    add: Add Controller For Retrive All Cart And Cart By Id

--- app/controllers/cart.js
+++ app/controllers/cart.js
@@ -1,7 +1,23 @@
+/** ***********************************************************************
+ * Execution        : 1. default node       cmd> nodemon server.js
+ *
+ * Purpose          : to hit the perticular API
+
+ * @file            : cart.js
+ * @author          : Pranil Kakade
+ * @version         : 1.0
+ * @since           : 17-06-2021
+ ************************************************************************* */
 const cartService = require('../services/cart');
 
 class CartController {
 
+  /**
+  * @description : It is adding book to cart in bookstore
+  * @param {*} req
+  * @param {*} res
+  * @method : addToCart from service.js
+  */
   addToCart = (req, res) => {
         const cartData = {
             bookId: req.body.bookId,
@@ -17,13 +33,19 @@ class CartController {
             } else {
               return res.status(200).send({
                 success: true,
-                message: 'Book Added In A Cart Successfully...!!! ',
+                message: 'Book Added In A Cart Successfully...!!!',
                 data: addCart, 
                });
             }
           }); 
   };
 
+  /**
+   * @description : It is remove book from cart
+   * @param {*} req
+   * @param {*} res
+   * @method: removeFromCart from service.js
+  */
   removeFromCart = (req, res) => {
     const cartDetail = {
       bookId: req.body.bookId,
@@ -46,23 +68,102 @@ class CartController {
     });
   };
 
-  purchaseBook = (req, res) => {
-    const cart = {
-     cartId: req.body.cartId,
-     userId: req.userId,
-    }
-    cartService.purchaseBook(cart).then(() => {
-      res.status(200).send({
-        success: true,
-        message: 'order purchase successfully',
-      });
-    }).catch((err) => {
-      res.status(400).send({
+  /**
+   * @description : It is remove book from cart
+   * @param {*} req
+   * @param {*} res
+   * @method: removeFromCart from service.js
+  */
+  purchaseBook = async (req, res) => {
+    try {
+      const cart = {
+        cartId: req.body.cartId,
+        userId: req.userId,
+       }
+       const cartResult = await cartService.purchaseBook(cart);
+       if(cartResult.data == null || cartResult.data == undefined || cartResult.data == '') {
+        return res.status(401).send({
+          success: false,
+          message: 'There is No Cart Id Associate With Log in User...!',
+        }); 
+       } else {
+        return res.status(200).send({
+          success: true,
+          message: 'Purchase Book From Cart successfully..!',
+          data: cartResult,
+        });
+       }
+    } catch (error) {
+      return res.status(500).send({
         success: false,
-        message: 'unable to purchase order',
-        err,
+        message: 'Internal Server Error...!',
+        error,
+      });
+    }
+         
+  };
+
+  /**
+   * @description : It is getting all existing carts from bookStore
+   * @param {*} req
+   * @param {*} res
+   * @method: getAllCarts from service.js
+  */
+  getAllCart = async (req, res) => {
+    try {
+      const getCart = await cartService.getAllCart();
+      if(getCart !== null){
+        return res.status(200).send({
+            success: true,
+            message: 'All Carts Retrived Successfully....!!!',
+            data: getCart,
+        });
+    } else {
+        return res.status(401).send({
+            success: false,
+            message: 'Failed To Retrive All Carts.....!!',
+        });
+      } 
+    } catch (error) {
+      return res.status(500).send({
+        success: false,
+        message: 'Internal Server Error...!',
+        error,
+      });
+    }
+  };
+
+  /**
+   * @description : It is getting a existing cart from bookStore
+   * @param {*} req
+   * @param {*} res
+   * @method: getCartById from service.js
+  */
+  getCartById = async (req, res) => {
+    try {
+      const getCartId = req.params.cartId;
+
+    const cartDataId = await cartService.getCartById(getCartId);
+    if (cartDataId !== null) {
+      return res.status(200).send({
+        success: true,
+        message: 'Retrived A Perticular Cart Successfully....!!!' + getCartId,
+        data: cartDataId,
     });
-    })
+    } else {
+      return res.status(401).send({
+        success: false,
+        message: 'Failed To Retrive A Cart.....!!',
+    });
+  }
+    } catch (error) {
+      return res.status(500).send({
+        success: false,
+        message: 'Internal Server Error...!',
+        error,
+      }); 
+    }
+    
   };
 };
 
