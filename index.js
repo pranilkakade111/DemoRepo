a8862226a662c004196ff5798e5c16717835c72c
Pranil Kakade <pranilkakade111@gmail.com>
2021-07-10 20:35:08 +0530

    add: socket.io For Price Filter API For Enabled Real-Time Data In Book Store App

--- index.js
+++ index.js
@@ -1,28 +1,36 @@
 const express = require('express');
 const axios = require('axios');
+const socket = require('socket.io');
 
 const app = express();
 const baseUrl = 'http://localhost:8000';
 
-app.use(express.static(`${__dirname}/public`));
+app.use(express.static(`${__dirname}/public/`));
 
 // listen for request
 const server = app.listen(4000, () => {
   console.log('Connected, server started listening on port :', 4000);
 });
 
-const io = require('socket.io')(server);
+const io = socket(server);
 
-function getData(payload) {
-  console.log('axios', payload);
-  return axios.post(`${baseUrl}/books/priceRange`, payload);
-}
+/**
+  * @description this function takes books cost ranges as a parameter and
+  * returns number of books, corrousponding ranges
+  * @param {*} payload contains the books cost ranges
+  */
+const getData = (payload) => axios.post(`${baseUrl}/books/priceRange`, payload);
+
+/**
+ * @description establish connection with client, emit response as per
+ * requested event by client
+ * @param socket is reponsible to emit and on envets
+ */
 io.on('connection', (socket) => {
-  console.log('connected with soket..!');
+  console.log('connected with soket');
   socket.on('range', async (payload) => {
     let result = [];
     result = await getData(payload);
-    console.log('result', result);
-    socket.emit('range', result.data);
+    socket.emit('range', result.data.data);
   });
 });
